58aff87ebbae463e68af46495300c8990d993523
NSa <nino.sandmeier@web.de>
2024-06-13 22:38:55 -0400

    Refactoring to class based design

---
+++ BusCrawler/newVenv/BusPlatformCrawler/website_crawler_abstract.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from enum import Enum
+from pandas import DataFrame as df
+
+class Debug(Enum):
+    
+    NO_DEBUG = 0
+    LOCAL_SELENIUM_CONTAINER = 1
+    LOCAL_CHROME_INSTANCE = 2
+
+class Crawler(ABC):
+
+    def __init__(self, url: str, origin_city: str, destination_city:str, date: str):
+        self._url = url
+        self.origin_city = origin_city
+        self.destination_city = destination_city
+        self.date = date
+        self._main_logging_string = f"[{self.origin_city} to {self.destination_city}]: "
+        self.result_frame = None
+        self.final_origin = None
+        self.final_destination = None
+        self.debug = Debug.NO_DEBUG
+
+    @abstractmethod
+    def retrieve_info() -> df:
+        pass
+
+    def _build_result_dict(self) -> dict:
+        return {"exits": [], "arrivals": [], "price": [], "currency": [], "availability": [], "transportclass": [], "origin": [], "destination": []}
+    
+    def connect_chrome(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('log-level=3')
+
+        if self.debug == Debug.NO_DEBUG:
+            driver = webdriver.Remote('http://selenium:4444/wd/hub', options=chrome_options)
+        elif self.debug == Debug.LOCAL_SELENIUM_CONTAINER:
+            driver = webdriver.Remote('http://localhost:4444', options=chrome_options)
+        elif self.debug == Debug.LOCAL_CHROME_INSTANCE:
+            driver = webdriver.Chrome(options= chrome_options)
+            
+        return driver
+
+    def _fix_date(self, date: str) -> str:
+        return date.replace('Mar','Martes').replace('hs','')
+
+    def print_result(self):
+        print(f"------------------\nOptions from {self.final_origin} to {self.final_destination} at {self.date}:\n", flush=True)
+        print(self.result_frame, flush=True)
+        print("\n------------------", flush=True)
--- BusCrawler/newVenv/DataCrawler/main.py
+++ BusCrawler/newVenv/DataCrawler/main.py
@@ -1,10 +1,13 @@
 import logging
-from BusPlatformCrawler.plataforma10_crawler import retrieve_plataforma10_info
+from BusPlatformCrawler.plataforma10_crawler import plataforma10_crawler
 import pandas as pd
 from pandas import DataFrame as df
 from datetime import date, timedelta
 import _pydevd_bundle.pydevd_constants
-_pydevd_bundle.pydevd_constants.PYDEVD_WARN_EVALUATION_TIMEOUT = 10
+_pydevd_bundle.pydevd_constants.PYDEVD_WARN_EVALUATION_TIMEOUT = 20
+from BusPlatformCrawler.website_crawler_abstract import Debug
+
+DEBUG = Debug.LOCAL_SELENIUM_CONTAINER
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(processName)s - %(levelname)s - %(message)s')
@@ -17,17 +20,20 @@ def worker_wrapper(args):
     try:
         start_message = f"Worker started for {args}"
         logging.info(start_message)
-        result = retrieve_plataforma10_info(*args)
+        crawler = plataforma10_crawler(*args)
+        crawler.debug = DEBUG
+        result = crawler.retrieve_info()
         end_message = f"Worker finished for {args}"
         logging.info(end_message)
-        return result
+        return [result, args]
     except Exception as e:
         print(f"Error: {e}", flush=True)
         return None
 
-def serialize(data: df):
+def serialize(data: df, table_appendix: str=''):
     from sqlalchemy import create_engine
     import pyodbc
+    assert isinstance(data, df), 'Serialization without data frame not possible!'
     # Verbindung zur Azure SQL Database herstellen
     server = 'busdata.database.windows.net'
     database = 'BusDataBase'
@@ -39,8 +45,8 @@ def serialize(data: df):
     engine = create_engine(database_url)
 
     # DataFrame in die SQL-Datenbank schreiben
-    today = date.strftime(date.today(), '%d/%m/%Y')
-    data.to_sql(today, con=engine, if_exists='replace', index=False)
+    table_name = date.strftime(date.today(), '%d/%m/%Y') + table_appendix
+    data.to_sql(table_name, con=engine, if_exists='replace', index=False)
 
 
 if __name__ == '__main__':
@@ -58,16 +64,28 @@ if __name__ == '__main__':
     searchdate = date.strftime(date.today() + timedelta(days=1), '%d/%m/%Y')
     results = []
 
-#    cities_combinations = cities_combinations[0:20]
+    cities_combinations = cities_combinations[0:3]
     
-    with multiprocessing.Pool(processes=10) as pool:
+    with multiprocessing.Pool(processes=1) as pool:
             inputs = [[c[0], c[1], searchdate] for c in cities_combinations]
-            for new_result in pool.imap_unordered(worker_wrapper, inputs):
+            cities_output_logging = []
+            for output in pool.imap_unordered(worker_wrapper, inputs):
+                new_result = output[0]
+                cities_output = output[1][0:2]
                 if new_result is None or (type(new_result) is list and new_result[0] is None):
-                    continue
-                if len(results) == 0:
-                    results = pd.concat([new_result])
+                    cities_output.append(0)
+                elif type(new_result) is not df and new_result == -1:
+                    cities_output.append(-1)
+                elif type(new_result) is not df and new_result == -2:
+                    cities_output.append(-2)
                 else:
-                    results = pd.concat([results,new_result])
+                    if len(results) == 0:
+                        results = pd.concat([new_result])
+                    else:
+                        results = pd.concat([results,new_result])
+                    cities_output.append(1)
+                cities_output_logging.append(cities_output)
     serialize(results)
+    cities_output_logging_dataframe = df(cities_output_logging,columns=['Origin', 'Destination', 'Status'])
+    serialize(cities_output_logging_dataframe, '_input_logging')
     print(results)
